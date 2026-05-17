bb5d3c4879852baf0cd3ce4602f38a2f58a7a86d
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-17 13:50:53 +0400

    add  variables

---
+++ clinicSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace clinicSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string PatientName = "";
+
+            int PatientID = 0;
+
+            int PatientAge = 0;
+
+            string PatientPhone = "";
+
+
+
+            
+
+
+
+        }
+    }
+}
