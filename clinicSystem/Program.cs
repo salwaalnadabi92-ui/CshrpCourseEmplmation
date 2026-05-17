9408a2d33d08c75acdb4558ca62cea5979add001
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-17 14:11:47 +0400

    implement switch

--- clinicSystem/Program.cs
+++ clinicSystem/Program.cs
@@ -14,12 +14,80 @@
 
             string PatientPhone = "";
 
+            bool IsActive = false;
 
 
-            
+        
+           
+
+                Console.WriteLine("Main Menu");
+            Console.WriteLine("1)Register patient");
+            Console.WriteLine("2) Veiw patient");
+            Console.WriteLine("3)Update patient Information");
+            Console.WriteLine("4)delete patient");
+            Console.WriteLine("5)Medical Registor");
+            Console.WriteLine("6)EXIT");
+
+
+                Console.WriteLine("Enter your Opation");
+                int opation = int.Parse(Console.ReadLine());
+
+
+                switch (opation)
+                {
+
+              
+
+                case 1:
+                        Console.WriteLine("1)Register patient");
 
 
 
+
+
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("2) Veiw patient");
+
+                        break;
+
+
+
+                    case 3:
+                        Console.WriteLine("3)Update patient Information");
+
+                        break;
+
+
+                       case 4:
+                        Console.WriteLine("4)delete patient");
+
+
+                        break;
+
+                       case 5 :
+                        Console.WriteLine("5)Medical Registor");
+
+
+                        break;
+
+
+                    case 6:
+
+                        Console.WriteLine("6)EXIT");
+
+
+                        break;
+
+
+                } // switch
+
+
+
+
+
+            }
         }
-    }
 }
