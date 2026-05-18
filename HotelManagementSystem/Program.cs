1558e5e8c3c1144c3ed3f11b666596b362d7f04d
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-18 15:20:51 +0400

    add variable types

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace HotelManagementSystem
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HotelManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //System storge
+
+            string guestName = "";
+            string guestPhone = "";
+            int    roomNumber = 0;
+            string roomType = "";
+            double nightlyRate = 0;
+            string checkinDate = "";
+
+            string checkout_date = "";
+            int     number_Of_Nights = 0;
+            string roomNotes ="";
+            double discountPercentage = 0;
+
+           double  loyaltyPoints = 0;
+            bool guestIsRegistered = false;
+           bool    currentlyCheckedIn = false;
+
+
+
+
+
         }
     }
 }
