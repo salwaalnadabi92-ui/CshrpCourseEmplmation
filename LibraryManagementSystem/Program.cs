7f2634ce9369c5657ecd7c64031c0580d2cb19fc
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-20 09:23:18 +0400

    add project  ibraryManagementSystem

---
+++ LibraryManagementSystem/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Timers;
+using static System.Collections.Specialized.BitVector32;
+
+namespace LibraryManagementSystem
+{
+    internal class Program
+    {
+        //system storage(variables )
+
+        static string MemberName = "";
+
+        static int memberID = 0;
+
+        static string memberEmail ="";
+
+        static DateTime membershipExpiryDate = 0;
+
+        static string memberTier = "";
+
+        static bool   MemberIsregistered=false;
+
+        static string bookTitle = "";
+
+        static string bookAuthor = "";
+
+        static string bookGenre = "";
+
+        static string NumAvailableCopies = 0;
+
+        static bool   bookIsRegistered = false;
+
+        static int totalBooksBorrowed = 0;
+
+        static double totalFinesPaid = 0;
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
